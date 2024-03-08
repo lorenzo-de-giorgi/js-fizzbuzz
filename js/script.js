447c4e2fd5226a98175a378e6e36d2aafa67485f
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-08 15:27:15 +0100

    exercise completed

--- js/script.js
+++ js/script.js
@@ -5,12 +5,21 @@ square.className = 'd-flex justify-content-between align-content-center flex-wra
 //assegno un id al div
 square.setAttribute('id','boxes');
 // definisco il numero dei box da mostrare
-let numBox = 100;
+let numBox = 101;
 let tmpHtml = '';
 // ciclo for per creare i 100 quadrati
 for(let i = 0; i < numBox; i++){
-    tmpHtml += ` <div class="box">${i + 1}</div>`;
+    if(i % 3 == 0 && i % 5 == 0){
+        tmpHtml += ` <div class="box fizzbuzz-box">${i}</div>`;
+    } else if(i % 3 == 0){
+        tmpHtml += ` <div class="box fizz-box">${i}</div>`;
+    } else if(i % 5 == 0){
+        tmpHtml += ` <div class="box buzz-box">${i}</div>`;
+    } else {
+        tmpHtml += ` <div class="box">${i}</div>`;
+    }
 }
+
 square.innerHTML = tmpHtml;
 console.dir(square);
 // display with append
